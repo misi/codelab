a447ca8b0b96923f46ccb2f5e17c4f11c83a3f73
Mészáros Mihály <misi@odu.duckdns.org>
2018-02-20 09:18:48 +0000

    make it more logical and extend with DC init options

--- lab6/js/main.js
+++ lab6/js/main.js
@@ -17,27 +17,27 @@ var pc2 = new RTCPeerConnection(rtcConfig);
 var dc1;
 var dc2;
 
-dc1 = pc1.createDataChannel("chat");
+/* (Un)Ordered / (Un)Reliable etc.
+ * odrderd: If ordered set to false, data is allowed to be delivered out of order.
+ * maxRetransmits: Limits the number of times a channel will retransmit data if not successfully delivered.
+ * maxPacketLifeTime: Limits the time (in milliseconds) during which the channel will transmit or retransmit data if not acknowledged.
+ * priority: very-low to high  See: https://www.w3.org/TR/webrtc/#dom-rtcprioritytype
+ * binaryType: "blob" / "arraybuffer"
+ */
+const dcInitOptions = {
+	ordered: true,
+	maxRetransmits: 65535,
+	//maxPacketLifeTime: 65535,
+        priority: "low",
+	binaryType: "blob",
+};
 
-pc1.onicecandidate = function(e){
-  if (e.candidate) {
-    try {
-      pc2.addIceCandidate(e.candidate);
-    } catch (er) {
-      console.error("AddCandidateError: "+e);
-    }
-  }
-}
+dc1 = pc1.createDataChannel("chat",dcInitOptions);
 
-pc2.onicecandidate = function(e){
-  if (e.candidate) {
-    try {
-      pc1.addIceCandidate(e.candidate);
-    } catch (er) {
-      console.error("AddCandidateError: "+e);
-    }
-  }
-}
+
+dc1.onmessage = function(e) {
+  log1.value += e.data;
+};
 
 pc2.ondatachannel = function(e){
   dc2=e.channel;
@@ -47,6 +47,44 @@ pc2.ondatachannel = function(e){
 
 };
 
+input1.onkeypress = function (e){
+  if (e.keyCode === 13 || e.which === 13) {
+    var msg = "Alice: "+input1.value+"\n";
+    log1.value += msg;
+    dc1.send(msg);
+    input1.value="";
+  }
+};
+
+input2.onkeypress = function (e){
+  if (e.keyCode === 13 || e.which === 13) {
+    var msg = "Bob: "+input2.value+"\n";
+    log2.value += msg;
+    dc2.send(msg);
+    input2.value="";
+  }
+};
+
+pc1.onicecandidate = function(e){
+  if (e.candidate) {
+    try {
+      pc2.addIceCandidate(e.candidate);
+    } catch (er) {
+      console.error("AddCandidateError: "+e);
+    }
+  }
+};
+
+
+pc2.onicecandidate = function(e){
+  if (e.candidate) {
+    try {
+      pc1.addIceCandidate(e.candidate);
+    } catch (er) {
+      console.error("AddCandidateError: "+e);
+    }
+  }
+};
 
 try {
     pc1.createOffer()
@@ -57,28 +95,4 @@ try {
     .then(() => pc1.setRemoteDescription(pc2.localDescription));
 } catch(er){
   console.error("Error during Offer/Answer negotiation: "+er);
-}
-
-
-dc1.onmessage = function(e) {
-  log1.value += e.data;
-}
-
-
-input1.onkeypress = function (e){
-  if (e.keyCode === 13 || e.which === 13) {
-    var msg = "Alice: "+input1.value+"\n";
-    log1.value += msg;
-    dc1.send(msg);
-    input1.value="";
-  }
-}
-
-input2.onkeypress = function (e){
-  if (e.keyCode === 13 || e.which === 13) {
-    var msg = "Bob: "+input2.value+"\n";
-    log2.value += msg;
-    dc2.send(msg);
-    input2.value="";
-  }
-}
+};
