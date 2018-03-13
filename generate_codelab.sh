17e7822a83a076c44bff54c4bb07f7a15461dc65
Mészáros Mihály <misi@niif.hu>
2018-03-13 07:30:39 +0100

    add --no-backup-if-mismatch to not generate .orig file

--- generate_codelab.sh
+++ generate_codelab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ../../go/bin/claat export -ga UA-114682180-1  -prefix ./ 1o6q4_pPdqwHuvJhOTWVcdNTkDIC6xKz7nU5KkSdoBqc
-patch -p1  <source/index.html.patch
+patch --no-backup-if-mismatch -p1  <source/index.html.patch
